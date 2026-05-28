dffb0e0cab5aebcba6edd71207e39936fa381354
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-28 16:03:08 +0400

    project setup

---
+++ src/main/java/az/shopery/backoffice_ms/BackofficeMsApplication.java
@@ -0,0 +1,13 @@
+package az.shopery.backoffice_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/backoffice_ms/BackofficeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.backoffice_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
